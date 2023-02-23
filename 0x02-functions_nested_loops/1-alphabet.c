0f3554014e6869cd2f90be42d997434d580f3766
Anati <anati.sukantak@gmail.com>
2023-02-22 22:38:44 -0800

    output alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - print all aphabet in lowercase
+ *
+ * /
+ 
+void print_alphabet_x10(void)
+{  
+         char letter;
+	 int a;
+	 while (int a = 0; a < 10; a++) {
+	        for (lketter = 'a'; letter <= 'z'
