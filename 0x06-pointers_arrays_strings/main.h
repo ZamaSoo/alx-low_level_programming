05735177f772dafe4c644638fdf29c9e9e156601
Anati <anati.sukantak@gmail.com>
2023-03-02 10:38:34 -0800

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+Prototype: char *_strcat(char *dest, char *src);
+Prototype: char *_strncat(char *dest, char *src, int n);
+Prototype: char *_strncpy(char *dest, char *src, int n);
+Prototype: int _strcmp(char *s1, char *s2);
+Prototype: void reverse_array(int *a, int n);
+Prototype: char *string_toupper(char *);
+Prototype: char *cap_string(char *);
+
